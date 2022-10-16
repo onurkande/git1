8e71879d9a195b67f17be2cb9a5d7f84e0603c97
onurkande <108400849+onurkande@users.noreply.github.com>
2022-10-16 23:43:49 +0300

    blog migraiton

--- database/migrations/2022_10_16_203541_create_blogs_table.php
+++ database/migrations/2022_10_16_203541_create_blogs_table.php
@@ -15,6 +15,7 @@ class CreateBlogsTable extends Migration
     {
         Schema::create('blogs', function (Blueprint $table) {
             $table->id();
+            $table->string('text');
             $table->timestamps();
         });
     }
